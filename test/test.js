0e1668aced6bad7e54423e98b174a0686e6e3bb2
Maximilian Brandau <mbrandau@online.de>
2017-09-04 12:47:36 +0200

    Add unit testing

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const assert = require('assert'),ipWhitelist = require('../index'),path = require('path');
+describe('ip-whitelist', function() {
+    describe('#array()', function() {
+        const cb = ipWhitelist.array(['127.0.0.1','::1']);
+        it('should return true when the ip is 127.0.0.1', () => assert.equal(true, cb('127.0.0.1')));
+        it('should return true when the ip is ::1', () => assert.equal(true, cb('::1')));
+        it('should return false when the ip is 127.0.0.2', () => assert.equal(false, cb('127.0.0.2')));
+        it('should return false when the ip is ::2', () => assert.equal(false, cb('::2')));
+    });
+    describe('#file()', function() {
+        const cb = ipWhitelist.file(path.join(__dirname,'ips.txt'));
+        it('should return true when the ip is 127.0.0.1', () => assert.equal(true, cb('127.0.0.1')));
+        it('should return true when the ip is ::1', () => assert.equal(true, cb('::1')));
+        it('should return false when the ip is 127.0.0.2', () => assert.equal(false, cb('127.0.0.2')));
+        it('should return false when the ip is ::2', () => assert.equal(false, cb('::2')));
+    });
+});
